05daf808ae927b0eb0074b1523db245704a50a0c
vidhuprakashTechfriar <160308006+vidhuprakashTechfriar@users.noreply.github.com>
2024-05-18 13:51:59 +0530

    ---client side search --added

--- client/src/components/sideBar/searchInput/SearchInput.jsx
+++ client/src/components/sideBar/searchInput/SearchInput.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import useConversation from "../../../zustand-store/useConversation";
+import useGetConversation from "../../../hooks/useGetConversation";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversation } = useGetConversation();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search must be 3 character long");
+    }
+    const foundconversation = conversation.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (foundconversation) {
+      setSelectedConversation(foundconversation);
+      setSearch("");
+    } else {
+      toast.error("No conversation found");
+    }
+  };
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full"
